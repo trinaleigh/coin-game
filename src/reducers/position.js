e3886d6483ba9b1b56ebb1f26c90ff82b4c14188
trinaleigh <trinaleigh@users.noreply.github.com>
2017-03-15 11:38:29 -0400

    constrain movement

--- src/reducers/position.js
+++ src/reducers/position.js
@@ -1,6 +1,9 @@
 const position = (state = {x:0, y:0}, action) => {
 	if (action.type === 'MOVE_PLAYER') {
-        return {x: state.x + action.x, y: state.y + action.y}
+		var xMove = state.x + action.x
+		var yMove = state.y + action.y
+        return {x: xMove > 0 && xMove < 10 ? xMove : 0, 
+        		y: yMove > 0 && yMove < 10 ? yMove : 0}
 	} else {
 		return state
 	}
